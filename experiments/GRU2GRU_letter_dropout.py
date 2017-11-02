6173b1bc0e74532832d038f24ad742c76fa6a2d1
kcrong <kcrong@kim82536.pe.kr>
2017-11-02 10:39:48 +0900

    Fit dropout model to without_dropout model

--- experiments/GRU2GRU_letter_dropout.py
+++ experiments/GRU2GRU_letter_dropout.py
@@ -48,8 +48,7 @@ english2idx = {v: k for k, v in idx2english.items()}
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('data/demo_data.csv', encoding='utf8')
-del df['Unnamed: 0']
+df = pd.read_csv('data/full_data.csv', encoding='utf8')
 
 
 # Korean data
@@ -85,7 +84,7 @@ y[np.isnan(y)] = 0
 
 # one hot encoding
 from keras.utils import np_utils
-y = np_utils.to_categorical(y, num_classes=len(korean_letter_list)+1).reshape(300, 12, len(korean_letter_list)+1)
+y = np_utils.to_categorical(y, num_classes=len(korean_letter_list)+1).reshape(y.shape[0], y.shape[1], len(korean_letter_list)+1)
 
 
 # Split train & test & val set
@@ -106,15 +105,15 @@ INVERT = True
 HIDDEN_SIZE = 200
 BATCH_SIZE = 100
 LAYERS = 2
-MAX_EPOCHS = 1000
-DROPOUT_RATE = 0.25
+MAX_EPOCHS = 100
 EMBEDDING_OUTPUT_SIZE = 128
 MAX_OUTPUT_SENT_LENGTH = y.shape[1]
+DROPOUT_RATE = 0.25
 
 RNN = recurrent.GRU
 stop_monitor = 'val_acc'
 stop_delta = 0.0
-stop_epochs = 30
+stop_epochs = 20
 
 
 # ## Build Model
@@ -122,7 +121,7 @@ stop_epochs = 30
 # In[25]:
 
 
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, TimeDistributed, Activation, RepeatVector, Embedding, Dropout
 
 
@@ -161,11 +160,13 @@ model.summary()
 print('Done')
 
 
-## Early Stop
-from keras.callbacks import EarlyStopping
+# Callbacks
+from keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
 
 ## Visualize keras training status
 from keras_tqdm import TQDMCallback
+
+log_dir = './logs/' + file_name
 
 callbacks_list = [
     EarlyStopping(
@@ -178,6 +179,16 @@ callbacks_list = [
     TQDMCallback(
         leave_inner=False,
         leave_outer=True
+    ),
+    TensorBoard(
+        log_dir=log_dir
+    ),
+    ModelCheckpoint(
+        filepath='./models/' + file_name + '_checkpoint',
+        monitor=stop_monitor,
+        save_best_only=True,
+        verbose=1,
+        mode='auto',
     ),
 ]
 
@@ -217,12 +228,24 @@ acc_ax.legend(loc='lower left')
 
 image_path = 'results/' + file_name + '.png'
 fig.savefig(image_path)
+print()
 print('Save graph at: ', image_path)
 
 
 loss, acc = model.evaluate(X_test, y_test)
 
+model.save('./models/' + file_name + '_latest')
 print()
+print("Save model at: ", './models/' + file_name + '_latest')
+
+loaded_model = load_model('./models/' + file_name + '_checkpoint')
+loaded_loss, loaded_acc = loaded_model.evaluate(X_test, y_test)
+
+print()
+print("Last Model: ")
 print('Loss: %.2f' % loss)
 print('Accuracy: %.2f' % acc)
 
+print("Saved Best Model: ")
+print('Loss: %.2f' % loaded_loss)
+print('Accuracy: %.2f' % loaded_acc)
